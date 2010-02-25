7f22fa40ac141a709639e46a71300881fc128cce
Daniel Kaufman <daniel.kaufman@coloradocollege.edu>
2010-02-25 16:36:28 +0000

    Finished constants for Card class and made them public.  This will eases computation in the Deck class because it can now call things like "for i : Card.NUM_ATTRS"
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@10 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/Card.java
+++ src/Card.java
@@ -1,12 +1,28 @@
 
 public class Card {
-	private final int NUM_ATTRS = 4;
+	public static final int NUM_ATTRS = 4;
 	
 	int[] attributes = new int[NUM_ATTRS];
 	public static final int NUMBER = 0;
 	public static final int COLOR = 1;
 	public static final int TEXTURE = 2;
 	public static final int SHAPE = 3;
+	
+	public static final int ONE_SHAPE = 0;
+	public static final int TWO_SHAPE = 1;
+	public static final int THREE_SHAPE = 2;
+	
+	public static final int RED = 0;
+	public static final int GREEN = 1;
+	public static final int PURPLE = 2;
+	
+	public static final int HOLLOW = 0;
+	public static final int HASHED = 1;
+	public static final int SOLID = 2;
+	
+	public static final int OBLONG = 0;
+	public static final int DIAMOND = 1;
+	public static final int SQUIGGLE = 2;
 	
 	/*
 	 * Current Attributes:
