bdf3429f4c46d72fdd3b34b57a110a59521b5ff5
Daniel Kaufman <daniel.kaufman@coloradocollege.edu>
2010-02-25 16:28:22 +0000

    Adding some constants to the Card class
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@9 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/Card.java
+++ src/Card.java
@@ -3,6 +3,10 @@ public class Card {
 	private final int NUM_ATTRS = 4;
 	
 	int[] attributes = new int[NUM_ATTRS];
+	public static final int NUMBER = 0;
+	public static final int COLOR = 1;
+	public static final int TEXTURE = 2;
+	public static final int SHAPE = 3;
 	
 	/*
 	 * Current Attributes:
