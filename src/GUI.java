dc6d1fa10d5abd5e1689b7c0c055afb892ac7986
Adam Urban <adam.urban@coloradocollege.edu>
2010-02-28 02:09:27 +0000

    Started the GUI...
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@13 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class GUI {
-//TODO: Everything
+
+	private static final int numCards = 9;
+
+	private static void createAndShowGUI() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("P2PSet");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JButton[] cards = new JButton[numCards];
+		
+		//Add buttons to panel in as close to a square as we can
+		int numCols = (int)Math.sqrt(numCards); //cast result as an int to throw away decimal
+		int remainderCards = numCards % numCols;
+
+		int numRows = numCols;
+		if (remainderCards != 0) numRows++; //if we're not a perfect square, add a row for the leftovers
+
+		JPanel cardPanel = new JPanel(new GridLayout(numRows,numCols));
+
+		//Add buttons to panel
+		for (JButton i:cards)
+		{
+			i = new JButton();
+			cardPanel.add(i);
+		}//for
+
+		frame.add(cardPanel);
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println("This is the main method.  Yay!!!");
-	}
-	//This is another test...
+
+		createAndShowGUI();
+
+	}//main
 }
