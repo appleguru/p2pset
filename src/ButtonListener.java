0a16b134880673d49447af6cee77f3564bd574ef
Daniel Kaufman <daniel.kaufman@coloradocollege.edu>
2010-03-01 03:25:15 +0000

    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@25 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/ButtonListener.java
+++ src/ButtonListener.java
@@ -1,9 +1,23 @@
 import java.awt.event.*;
+import javax.swing.JToggleButton;
 
 public class ButtonListener implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 
+		JToggleButton pressed = (JToggleButton)e.getSource();
+
+		if (pressed.isSelected()){
+			P2PSet.selectedCards.add(pressed);
+			if (P2PSet.selectedCards.size() == 3){
+				//todo claim the set
+			}
+		}
+		
+		else {
+			P2PSet.selectedCards.remove(pressed);
+		}
+		
 	}
 
 }
