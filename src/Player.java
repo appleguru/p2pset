95e12918aea7523fc172ea18ba863f012c8d15ad
Adam Urban <adam.urban@coloradocollege.edu>
2010-03-01 06:38:55 +0000

    Added Constructor
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@55 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/Player.java
+++ src/Player.java
@@ -6,4 +6,16 @@ public class Player
 	public InetAddress ip;
 	public int score;
 	public boolean isServer;
+	
+	public Player()
+	{
+	score = 0;
+	name = "Me!"; //TODO: Get this from text field in GUI
+	
+	try { ip = InetAddress.getLocalHost(); }
+	catch (Exception e) { System.err.println("Error " + e + " getting local address."); }
+		
+	isServer = false;
+	}//Constructor
+	
 }
