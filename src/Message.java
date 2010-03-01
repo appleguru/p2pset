9b2280caf79dcbedd7be4a9478523a3883e1b198
Daniel Kaufman <daniel.kaufman@coloradocollege.edu>
2010-03-01 07:53:44 +0000

    Made Message Serializable
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@73 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/Message.java
+++ src/Message.java
@@ -1,7 +1,12 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Message
+public class Message implements Serializable
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String command;
 	private ArrayList<Object> objects;
 	
