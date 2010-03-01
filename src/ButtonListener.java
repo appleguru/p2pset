a9b2e1b53dda35eef89b0a64a7aad06168dabfee
Adam Urban <adam.urban@coloradocollege.edu>
2010-03-01 03:07:38 +0000

    Made Button Array an ivar
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@24 03465e91-bfe4-4b14-b54d-35190c706e85

--- src/ButtonListener.java
+++ src/ButtonListener.java
@@ -1,11 +1,9 @@
 import java.awt.event.*;
 
-
 public class ButtonListener implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/P2PSet.java
+++ src/P2PSet.java
@@ -8,6 +8,7 @@ public class P2PSet {
 	private static final int defaultWindowWidth = 640;
 	private static final int defaultWindowHeight = 480;
 	private static final int iconScaleWidth = 128; //TODO: Get this from current window size on resize... 
+	protected static JToggleButton[] cards;
 	
 
 	private static void createAndShowGUI() {
@@ -15,7 +16,7 @@ public class P2PSet {
 		JFrame frame = new JFrame("P2P Set");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JToggleButton[] cards = new JToggleButton[numCards];
+		cards = new JToggleButton[numCards];
 		
 		//Add buttons to panel in as close to a square as we can
 		int numCols = (int)Math.sqrt(numCards); //cast result as an int to throw away decimal
