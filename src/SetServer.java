a8a1905fa994b8c6caad90d67a5433149274ec6a
Ari Sapon-White <ari.saponwhite@coloradocollege.edu>
2010-03-01 03:34:57 +0000

    Changed name of class, since this will run when a peer is the server.
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@27 03465e91-bfe4-4b14-b54d-35190c706e85

---
+++ src/SetServer.java
@@ -0,0 +1,37 @@
+public class SetServer {
+
+	private static boolean isServer;
+	private String myName;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		GUI myGUI = new GUI();
+	}
+	
+	/**
+	 * Sets the server to the next player in the player list.
+	 * @return Returns the name of the new server.
+	 */
+	private String setServer()
+	{
+		return "";
+	}
+	/**
+	 * If this player is the server, this method can be called to broadcast its GameData object to all other players, forcing them to synchronize to it.
+	 */
+	private void synchronizeSend()
+	{
+		
+	}
+	/**
+	 * Synchronizes this player's GameData object to the incoming one.
+	 * @param gd The incoming GameData object from the server.
+	 */
+	private void synchronizeSelfTo(GameData gd)
+	{
+		
+	}
+
+}
