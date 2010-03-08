7d9e12589b17ddb333221a2ecaadd7023f423285
Ari Sapon-White <ari.saponwhite@coloradocollege.edu>
2010-03-08 01:05:03 +0000

    New class for sending and receiving messages.
    git-svn-id: svn://localhost/Users/aurban/p2pset/trunk@106 03465e91-bfe4-4b14-b54d-35190c706e85

---
+++ src/Communicator.java
@@ -0,0 +1,170 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class Communicator
+{
+	private static final String MULTICAST_ADDRESS = "225.6.6.6";
+	private static final int MULTICAST_PORT = 6262;
+	private static final int TCP_PORT = 2626;
+	private ArrayList<Player> players;
+	private Queue<Message> msgQueue;
+	private msgListener ml;
+	private multicastListener mcl;
+	
+	public Communicator()
+	{
+		mcl = new multicastListener(MULTICAST_PORT, MULTICAST_ADDRESS);
+		mcl.start();
+		ml = new msgListener(TCP_PORT);
+		ml.start();
+	}
+	
+	public void sendTCPMessage(Message m, String recipient)
+	{
+		try
+		{
+			Socket sock = new Socket(recipient, TCP_PORT);
+			ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+			oos.writeObject(m);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace(System.out);
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void sendMulticastMessage(String m)
+	{
+		try
+		{
+			MulticastSocket mcs = new MulticastSocket(MULTICAST_PORT);
+			byte[] buffer = "Hi guys!".getBytes();
+			DatagramPacket dp = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(MULTICAST_ADDRESS), MULTICAST_PORT);
+			mcs.send(dp);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace(System.out);
+		}
+	}
+	
+	public void sendTEST_MESSAGE()
+	{
+		
+	}
+	
+	public void sendI_CLAIM_SET()
+	{
+		
+	}
+	
+	public void sendLOOKING_FOR_GAMES()
+	{
+		
+	}
+	
+	public void sendHERE_IS_A_GAME()
+	{
+		
+	}
+	
+	public void sendNEW_PLAYER()
+	{
+		
+	}
+	
+	public void sendSYNCHRONIZE_TO_ME()
+	{
+		
+	}
+	
+	public void sendPASS_TOKEN()
+	{
+		
+	}
+	
+	public Message receiveMsg()
+	{
+		return msgQueue.poll();
+	}
+	
+	
+	
+	public class msgListener extends Thread
+	{
+		private int port;
+		private ServerSocket ss;
+		
+		public msgListener(int _port)
+		{
+			port = _port;
+			ss = null;
+		}
+		
+		public void run()
+		{
+			try
+			{
+				ss = new ServerSocket(port);
+				while(true)	//Main loop
+				{
+					Socket cs = ss.accept();	//Wait for a connection
+					ObjectInputStream ois = new ObjectInputStream(cs.getInputStream());
+					Message m = (Message)ois.readObject();
+					cs.close();
+					msgQueue.add(m);
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace(System.out);
+			}
+		}
+	}
+
+	public class multicastListener extends Thread
+	{
+		private int port;
+		private String address;
+		private MulticastSocket mcs;
+
+		public multicastListener(int _port, String _address)
+		{
+			port = _port;
+			address = _address;
+		}
+		
+		public void run()
+		{
+			try
+			{
+				mcs = new MulticastSocket(port);
+				mcs.joinGroup(InetAddress.getByName(address));
+				while(true)
+				{
+					byte[] buffer = new byte[8];
+					DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+					mcs.receive(dp);
+					String commandString = new String(dp.getData());
+					String senderIP = dp.getAddress().getHostAddress();
+					ArrayList<Object> objArray = new ArrayList<Object>();
+					objArray.add(senderIP);
+					Message m = new Message(commandString, objArray);
+					msgQueue.add(m);
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
